98ad0a6a2d0c5c92a0c40192373031182f6b49d7
arman <arman@noca.pro>
2025-09-08 08:06:27 +0700

    update tsup.config.ts for typesafety

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  treeshake: false,
+  minify: false,
+  target: 'es2020',
+  outDir: 'dist',
+  bundle: false,
 });
